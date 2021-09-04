cd7c2a7d0201d02d4b4c81dff3c6e16dd31f7317
haris siddiqui <haaris21796@gmail.com>
2021-09-05 00:02:26 +0530

    adding an updated app.java file having coding

---
+++ app.java
@@ -0,0 +1,20 @@
+[200~package Abstraction;
+
+public class Employee extends Customer {
+	private String empId;
+
+	public String getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+
+	public String getCustomerInfo() {
+		return String.format("Customer: %s, EmpId: %s, Address: %s", getName(), getEmpId(), getAddress());
+
+	}
+
+}
+
